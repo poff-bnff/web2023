e159e5260eee6f8b35b9ea689ef34d69117a4853
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-06-03 17:31:19 +0300

    distill schema from poff datamodel and strapi file structure

--- shemaUpgradeV3toV4.js
+++ shemaUpgradeV3toV4.js
@@ -29,56 +29,135 @@ const strapi3Schema = {
       // console.log(`WARNING: ${modelName} has no settings.json`)
       return false
     })
-    .reduce((obj, modelName) => {
+    .reduce((collectionsArray, modelName) => {
       const model = JSON.parse(fs.readFileSync(path.join(STRAPI3_COLLECTIONS_PATH, modelName, 'models', `${modelName}.settings.json`), 'utf8'))
-      obj.push({
+      collectionsArray.push({
         kind: model.kind,
         modelName: modelName,
         collectionName: model.collectionName,
-        displayName: model.info.name
+        displayName: model.info.name,
+        attributes: model.attributes
       })
-      return obj
+      return collectionsArray
     }, []),
   components: STRAPI3_COMPONENTS
     .filter((componentGroupName) => fs.statSync(path.join(STRAPI3_COMPONENTS_PATH, componentGroupName)).isDirectory())
-    .reduce((componentsGroupObj, componentGroupName) => {
+    .reduce((componentsGroupArray, componentGroupName) => {
       const componentGroupPath = path.join(STRAPI3_COMPONENTS_PATH, componentGroupName)
       const componentFileNames = fs.readdirSync(componentGroupPath)
-      componentsGroupObj[componentGroupName] = componentFileNames.reduce((componentsObj, componentFileName) => {
+      const newComponentsGroupArray = componentFileNames
+      .reduce((componentsArray, componentFileName) => {
         const componentPath = path.join(componentGroupPath, `${componentFileName}`)
         const component = JSON.parse(fs.readFileSync(componentPath), 'utf8')
-        componentsObj[componentFileName] = {
+        const modelName = componentFileName.split('.')[0]
+        componentsArray.push({
           kind: 'componentType',
-          modelName: componentFileName,
+          componentGroupName: componentGroupName,
+          modelName: modelName,
           collectionName: component.collectionName,
           displayName: component.info.name
-        }
-        return componentsObj
-      }, {})
-      return componentsGroupObj
-    }, {})
+        })
+        return componentsArray
+      }, [])
+      return componentsGroupArray.concat(newComponentsGroupArray)
+    }, [])
 }
 
-fs.writeFileSync(path.join(__dirname, 'strapi3Schema.json'), JSON.stringify(strapi3Schema, null, 4))
 
 const strapi4Schema = {
-  collections: {},
-  components: {}
+  collections: [],
+  components: []
 }
 
-for (key in s3datamodel) {
-  const element = s3datamodel[key]
+const addCollectionToS4Schema = (s3Collection) => {
+  // hard copy s3Collection to s4Collection to avoid side effects
+  const s4Collection = {
+    kind: s3Collection.kind,
+    modelName: s3Collection.modelName,
+    collectionName: s3Collection.collectionName,
+    displayName: s3Collection.displayName,
+    s3DisplayName: s3Collection.s3DisplayName,
+    attributes: s3Collection.attributes
+  }
+  strapi4Schema.collections.push(s4Collection)
+  // console.log(`writeS4Collection: ${JSON.stringify(s3Collection, null, 2)}`)
+}
+
+const addComponentsToS4Schema = (componentNames) => {
+  componentNames.forEach((componentName) => {
+    // break componentName into componentGroupName and modelName
+    const [componentGroupName, modelName] = componentName.split('.')
+    // if component already in s4Schema, skip
+    if (strapi4Schema.components
+      .find((component) => {
+        return component.modelName === modelName
+          && component.componentGroupName === componentGroupName
+      })) {
+      return
+    }
+    // find component in s3Schema
+    const s3Component = strapi3Schema.components
+      .find((component) => {
+        return component.modelName === modelName
+          && component.componentGroupName === componentGroupName
+      })
+    if (!s3Component) {
+      console.log(`WARNING: component ${componentName} not found in s3Schema`)
+      return
+    }
+    // add component to s4Schema
+    addComponentToS4Schema(s3Component)
+  })
+}
+
+const addComponentToS4Schema = (s3Component) => {
+  // hard copy s3Component to s4Component to avoid side effects
+  const s4Component = {
+    kind: s3Component.kind,
+    componentGroupName: s3Component.componentGroupName,
+    modelName: s3Component.modelName,
+    collectionName: s3Component.collectionName,
+    displayName: s3Component.displayName,
+    s3DisplayName: s3Component.s3DisplayName,
+    attributes: s3Component.attributes
+  }
+  strapi4Schema.components.push(s4Component)
+}
+
+const getComponentNamesInCollection = (collection) => {
+  return Object.keys(collection.attributes)
+    .filter((attributeName) => collection.attributes[attributeName].type === 'component')
+    .map((attributeName) => collection.attributes[attributeName].component)
+    // make it unique
+    .filter((value, index, self) => self.indexOf(value) === index)
+}
+
+// enrich strapi3Schema.collections with displaynames from s3datamodel
+for (s3DislpayName in s3datamodel) {
+  const element = s3datamodel[s3DislpayName]
   if (element._path) {
     const modelName = element._path.split('/')[1]
     const collectionName = modelName.replace(/-/g, '_')
+    // console.log(`s3DislpayName: ${s3DislpayName}, modelName: ${modelName}, collectionName: ${collectionName}`)
     // find a collection in strapi3Schema.collections, where modelName or collectionName matches
-    const collection = strapi3Schema.collections.find((collection) => {
+    const s3Collection = strapi3Schema.collections.find((collection) => {
       return collection.modelName === modelName
         || collection.collectionName === collectionName
-        || collection.displayName === key
+        || collection.displayName === s3DislpayName
     })
-    console.log(`model: ${key}, modelName: ${modelName}, collectionName: ${collectionName}, collection: ${JSON.stringify(collection, null, 2)}`) // collection or singleType
+    if (s3Collection) {
+      s3Collection.s3DisplayName = s3DislpayName
+      addCollectionToS4Schema(s3Collection)
+      const componentNamesInCollection = getComponentNamesInCollection(s3Collection)
+      addComponentsToS4Schema(componentNamesInCollection)
+    } else {
+      console.log(`WARNING: ${s3DislpayName} not found in strapi3Schema.collections`)
+    }
+    // console.log(`model: ${s3DislpayName}, modelName: ${modelName}, collectionName: ${collectionName}, collection: ${JSON.stringify(collection, null, 2)}`) // collection or singleType
   } else {
-    console.log(`component: ${key}`)
+    // console.log(`WARNING: ${s3DislpayName} is a component`)
   }
 }
+
+fs.writeFileSync(path.join(__dirname, 'strapi3Schema.json'), JSON.stringify(strapi3Schema, null, 4))
+fs.writeFileSync(path.join(__dirname, 'strapi4Schema.json'), JSON.stringify(strapi4Schema, null, 4))
